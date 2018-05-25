948b4744b05cb17ddadaf21a70c1f1a6aaae2fc7
Ammar Bandukwala <ammar@ammar.io>
2018-05-25 09:20:05 -0500

    Remove useless Response string representation

--- response.go
+++ response.go
@@ -1,8 +1,6 @@
 package ipisp
 
 import (
-	"bytes"
-	"fmt"
 	"net"
 	"time"
 )
@@ -16,19 +14,4 @@ type Response struct {
 	Registry    string
 	Range       *net.IPNet
 	AllocatedAt time.Time
-}
-
-// String provides an easy to read version of r
-func (r *Response) String() string {
-	buf := &bytes.Buffer{}
-	buf.WriteRune('\n')
-	fmt.Fprintf(buf, "IP      : %v\n", r.IP)
-	fmt.Fprintf(buf, "ASN     : %v\n", r.ASN)
-	fmt.Fprintf(buf, "Name    : %v\n", r.Name)
-	fmt.Fprintf(buf, "Country : %v\n", r.Country)
-	fmt.Fprintf(buf, "Registry: %v\n", r.Registry)
-	fmt.Fprintf(buf, "Range   : %v\n", r.Range)
-	fmt.Fprintf(buf, "Alloc At: %v\n", r.AllocatedAt)
-
-	return buf.String()
 }
