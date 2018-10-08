9a928d77726f26c794ed7f6c2ee66170bf4af9e4
Ammar Bandukwala <ammar@ammar.io>
2018-10-08 16:00:47 -0500

    Support IPs with no ASN
    Resolves #10

--- asn.go
+++ asn.go
@@ -13,6 +13,11 @@ type ASN int
 
 // ParseASN parses a string like `AS2341` into ASN `2341`.
 func ParseASN(asn string) (ASN, error) {
+	// A special value from the API.
+	// More info: https://github.com/ammario/ipisp/issues/10.
+	if asn == "NA" {
+		return 0, nil
+	}
 	// Make case insensitive
 	asn = strings.ToUpper(asn)
 	if len(asn) > 2 && asn[:2] == "AS" {
@@ -23,7 +28,11 @@ func ParseASN(asn string) (ASN, error) {
 	return ASN(nn), errors.Wrap(err, "failed to conv into to string")
 }
 
+
 // String represents an ASN like `5544`` as `AS5544`.`
 func (a ASN) String() string {
+	if a == 0 {
+		return "N/A"
+	}
 	return "AS" + strconv.Itoa(int(a))
 }
