3acd12417243ebaf99a664e8de7285335ddb276f
Ammar Bandukwala <ammar@ammar.io>
2021-12-25 15:22:44 -0600

    Fix up example

--- example/main.go
+++ example/main.go
@@ -16,9 +16,9 @@ func main() {
 	}
 	fmt.Printf("resolved IP 4.2.2.2: %+v\n", resp)
 
-	resp, err = ipisp.LookupASN(ipisp.ASN(666))
+	resp, err = ipisp.LookupASN(context.Background(), ipisp.ASN(666))
 	if err != nil {
 		log.Fatalf("lookup ASN 666: %v", err)
 	}
-	fmt.Printf("resolved ASN 666: %+v\n", resp)
+	fmt.Printf("ISP: %+v\n", resp.ISPName)
 }
--- go.mod
+++ go.mod
@@ -1,7 +1,3 @@
 module github.com/ammario/ipisp/v2
 
-require github.com/ammario/ipisp v1.0.0
-
-require github.com/pkg/errors v0.8.0 // indirect
-
 go 1.17
